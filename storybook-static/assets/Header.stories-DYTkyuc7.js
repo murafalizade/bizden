1a73d6a5c2e018804de72f0e598be2a039f3fc97
muradeliyev <muradaliyev2229@gmail.com>
2025-05-20 23:45:28 +0400

    fix(storybook): change config storybook

---
+++ storybook-static/assets/Header.stories-DYTkyuc7.js
@@ -0,0 +1,7 @@
+import{f as o}from"./index-B7YJKKKT.js";import{H as p}from"./Header-B8MPTFuJ.js";import"./jsx-runtime-DoEZbXM1.js";import"./jsx-runtime-Bw5QeaCk.js";import"./Button-Bu-Zr1YM.js";import"./index-D_y78INj.js";import"./index-sVOi1kSy.js";const x={title:"Example/Header",component:p,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};var a,s,t;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  args: {
+    user: {
+      name: 'Jane Doe'
+    }
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,m,c;r.parameters={...r.parameters,docs:{...(n=r.parameters)==null?void 0:n.docs,source:{originalSource:"{}",...(c=(m=r.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};const H=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,H as __namedExportsOrder,x as default};
---
+++ storybook-static/assets/Login.stories-z8lM2Qho.js
@@ -0,0 +1,5 @@
+import{j as o}from"./jsx-runtime-DoEZbXM1.js";import"./jsx-runtime-Bw5QeaCk.js";const n=a=>o.jsx(o.Fragment,{children:"Login component"});n.__docgenInfo={description:"",methods:[],displayName:"Login"};const m={title:"(authless)/login/Login",component:n},e={args:{}};var r,s,t;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
+  args: {
+    // Add default args here
+  }
+}`,...(t=(s=e.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};const p=["Default"];export{e as Default,p as __namedExportsOrder,m as default};
